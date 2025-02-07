db0a1cb6ee98d1850bf1230a8db08a41cfa53fad
Lucas Gismondi <lucas@gismondigroup.com>
2025-02-07 08:04:34 -0500

    feat: adds border animation on page load

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Link } from "@/components/ui/link"
 function App() {
 
   return (
-    <div className="flex flex-col h-screen bg-border gap-[1px]">
+    <div className="flex flex-col h-screen gap-[1px]">
       <div className="flex flex-row justify-between p-2 bg-background">
         <p className="text-xs">Lucas Gismondi</p>
         <div className="flex flex-row gap-1">
@@ -15,17 +15,17 @@ function App() {
         <div className="flex w-full h-full border-animation bg-background">
           <p className="text-xs p-2">Experience</p>
         </div>
-        <div className="flex w-full h-full border-animation bg-background">
+        <div className="flex w-full h-full border-animation border-animation-delay-1 bg-background">
           <p className="text-xs p-2">Frontend</p>
         </div>
-        <div className="flex w-full h-full border-animation bg-background">
+        <div className="flex w-full h-full border-animation border-animation-delay-2 bg-background">
           <p className="text-xs p-2">Backend</p>
         </div>
-        <div className="flex w-full h-full border-animation bg-background">
+        <div className="flex w-full h-full border-animation border-animation-delay-3 bg-background">
           <p className="text-xs p-2">Cloud</p>
         </div>
       </div>
-      <div className="flex flex-row justify-between h-1/2 gap-[1px]">
+      <div className="flex flex-row justify-between h-1/2 gap-[1px] border-animation border-animation-delay-4">
         <div className="w-1/2 flex flex-col gap-[1px]">
           <div className="flex h-full w-full items-center justify-center p-2 bg-background">
             <p className="text-xs">About me</p>
@@ -38,7 +38,7 @@ function App() {
           <p className="text-xs">Interactive Globe</p>
         </div>
       </div>
-      <div className="flex flex-row justify-between p-2 bg-background">
+      <div className="flex flex-row justify-between p-2 bg-background border-animation border-animation-delay-5">
         <div className="flex flex-row gap-1">
           <Link className="text-xs" href="/">Github</Link>
           <Link className="text-xs" href="/">LinkedIn</Link>
