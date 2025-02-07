621327d277e945c8b8ad497541a7ff06ff85cced
Lucas Gismondi <lucas@gismondigroup.com>
2025-02-07 08:17:49 -0500

    feat: adds initial fade in animation

--- src/App.tsx
+++ src/App.tsx
@@ -5,47 +5,47 @@ function App() {
   return (
     <div className="flex flex-col h-screen gap-[1px]">
       <div className="flex flex-row justify-between p-2 bg-background">
-        <p className="text-xs">Lucas Gismondi</p>
-        <div className="flex flex-row gap-1">
+        <p className="text-xs fade-in">Lucas Gismondi</p>
+        <div className="flex flex-row gap-1 fade-in" style={{ animationDelay: '0.5s' }}>
           <Link className="text-xs" href="/">Resume</Link>
           <Link className="text-xs" href="/">Contact</Link>
         </div>
       </div>
       <div className="flex flex-col h-1/2 gap-[1px]">
         <div className="flex w-full h-full border-animation bg-background">
-          <p className="text-xs p-2">Experience</p>
+          <p className="text-xs p-2 fade-in" style={{ animationDelay: '0.1s' }}>Experience</p>
         </div>
         <div className="flex w-full h-full border-animation border-animation-delay-1 bg-background">
-          <p className="text-xs p-2">Frontend</p>
+          <p className="text-xs p-2 fade-in" style={{ animationDelay: '0.2s' }}>Frontend</p>
         </div>
         <div className="flex w-full h-full border-animation border-animation-delay-2 bg-background">
-          <p className="text-xs p-2">Backend</p>
+          <p className="text-xs p-2 fade-in" style={{ animationDelay: '0.3s' }}>Backend</p>
         </div>
         <div className="flex w-full h-full border-animation border-animation-delay-3 bg-background">
-          <p className="text-xs p-2">Cloud</p>
+          <p className="text-xs p-2 fade-in" style={{ animationDelay: '0.4s' }}>Cloud</p>
         </div>
       </div>
       <div className="flex flex-row justify-between h-1/2 gap-[1px] border-animation border-animation-delay-4">
         <div className="w-1/2 flex flex-col gap-[1px]">
           <div className="flex h-full w-full items-center justify-center p-2 bg-background">
-            <p className="text-xs">About me</p>
+            <p className="text-xs fade-in" style={{ animationDelay: '0.5s' }}>About me</p>
           </div>
           <div className="p-2 bg-background">
-            <p className="text-xs">Core principles</p>
+            <p className="text-xs fade-in" style={{ animationDelay: '0.6s' }}>Core principles</p>
           </div>
         </div>  
         <div className="w-1/2 flex justify-center items-center p-2 bg-background">
-          <p className="text-xs">Interactive Globe</p>
+          <p className="text-xs fade-in" style={{ animationDelay: '0.9s' }}>Interactive Globe</p>
         </div>
       </div>
       <div className="flex flex-row justify-between p-2 bg-background border-animation border-animation-delay-5">
-        <div className="flex flex-row gap-1">
+        <div className="flex flex-row gap-1 fade-in" style={{ animationDelay: '0.7s' }}>
           <Link className="text-xs" href="/">Github</Link>
           <Link className="text-xs" href="/">LinkedIn</Link>
           <Link className="text-xs" href="/">X</Link>
           <Link className="text-xs" href="/">Instagram</Link>
         </div>
-        <p className="text-xs">{`< ${new Date().getFullYear()} >`}</p>
+        <p className="text-xs fade-in" style={{ animationDelay: '1s' }}>{`< ${new Date().getFullYear()} >`}</p>
       </div>
     </div>
   )
